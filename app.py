cd690856bf96a50c7cdd49fa69c9a173442c3d88
Colonel Promsurin Phutthammawong <Colonel Promsurin Phutthammawong>
2017-03-26 00:35:59 +0700

    Try API bot

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, abort
+'''from flask import Flask, request, abort
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -50,4 +50,49 @@ def handle_message(event):
 def index():
     return 'OK ya!'
 if __name__ == "__main__":
-    app.run()
+    app.run()'''
+from flask import Flask, request
+import json
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello World!"
+# ส่วน callback สำหรับ Webhook
+@app.route('/callback', methods=['POST'])
+def callback():
+    json_line = request.get_json()
+    json_line = json.dumps(json_line)
+    decoded = json.loads(json_line)
+    user = decoded["events"][0]['replyToken']
+    #id=[d['replyToken'] for d in user][0]
+    #print(json_line)
+    print("ผู้ใช้：",user)
+    sendText(user,'งง') # ส่งข้อความ งง
+    return '',200
+
+def sendText(user, text):
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
+    Authorization = '888960969fc0ebb0bc365fc194e97dc9' # ใส่ ENTER_ACCESS_TOKEN เข้าไป
+
+    headers = {
+        'Content-Type': 'application/json; charset=UTF-8',
+        'Authorization':Authorization
+    }
+
+    data = json.dumps({
+        "replyToken":user,
+        "messages":[{
+            "type":"text",
+            "text":text
+        }]
+    })
+
+    #print("ข้อมูล：",data)
+    r = requests.post(LINE_API, headers=headers, data=data) # ส่งข้อมูล
+    #print(r.text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
